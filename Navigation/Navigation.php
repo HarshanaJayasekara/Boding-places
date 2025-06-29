0cffffc839bf315208e3d857f4aaa79454ec9d0f
Harshana Jayasekara <98016320+HarshanaJayasekara@users.noreply.github.com>
2025-06-30 01:10:53 +0530

    scss

---
+++ Navigation/Navigation.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #2C7865;">
+    <a class="navbar-brand font-weight-bold text-white" href="#">Rent</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link font-weight-bold text-white" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link font-weight-bold text-white" href="../BordingPlaces/register.php">Register <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-weight-bold text-white" href="../BordingPlaces/login.php">Login <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
